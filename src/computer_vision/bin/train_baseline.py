9fd0d6d2d8ac93daee314ede2a98e68f3f956e94
gcuder <gerald@itranslate.com>
2023-02-07 17:22:40 +0100

    Add flags

--- src/computer_vision/bin/train_baseline.py
+++ src/computer_vision/bin/train_baseline.py
@@ -1,14 +1,24 @@
+from absl import app, flags, logging  # noqa: E402
 
 from computer_vision.lab.augmentation import get_data_augmenter
 from computer_vision.lab.data import build_dataset
 from computer_vision.lab.models import baseline
+
 IMAGE_SIZE = (512, 512)
 
-
 def main():
-    train_ds, info = build_dataset(split='train', image_size=IMAGE_SIZE, batch_size=16)
-    dev_ds, _ = build_dataset(split='validation', image_size=IMAGE_SIZE, batch_size=16)
-    test_ds, _ = build_dataset(split='test', image_size=IMAGE_SIZE, batch_size=16)
+    FLAGS = flags.FLAGS
+
+    dataset_kwargs = dict(
+        dataset_card=FLAGS.dataset_card,
+        batch_size=FLAGS.batch_size,
+        data_dir=FLAGS.data_dir,
+        image_size=IMAGE_SIZE,
+        shuffle_buffer=FLAGS.shuffle_buffeer
+    )
+    train_ds, info = build_dataset(split='train', **dataset_kwargs)
+    dev_ds, _ = build_dataset(split='validation', **dataset_kwargs)
+    test_ds, _ = build_dataset(split='test', **dataset_kwargs)
 
     config = baseline.ClassifierWithBackboneConfig(num_classes=102)
     augmentation = get_data_augmenter()
@@ -20,14 +30,17 @@ def main():
         optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
     )
 
-    epochs = 10  # @param {type: "slider", min:10, max:100}
+    epochs = 10
     hist = model.fit(train_ds, epochs=epochs, validation_data=dev_ds)
     results = model.evaluate(test_ds, return_dict=True)
     print(results)
 
 
-
+def define_flags():
+    pass
 
 
 if __name__ == '__main__':
-    main()
+    logging.set_verbosity(logging.INFO)
+    define_flags()
+    app.run(main)
